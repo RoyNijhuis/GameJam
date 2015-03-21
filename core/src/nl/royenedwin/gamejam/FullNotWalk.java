a74ef53df45f08b0befa2ae61137f4da7ffd7193
RoyNijhuis <roynijhuis@hotmail.nl>
2015-03-21 15:50:32 +0100

    not walk

---
+++ core/src/nl/royenedwin/gamejam/FullNotWalk.java
@@ -0,0 +1,17 @@
+package nl.royenedwin.gamejam;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class FullNotWalk implements StaticObject{
+
+	@Override
+	public void render(SpriteBatch batch) {
+		
+	}
+
+	@Override
+	public void setPosition(Vector2 position) {
+		
+	}
+}
