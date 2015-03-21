29b39941953a85e953d103b98f9a1d494cef99d4
RoyNijhuis <roynijhuis@hotmail.nl>
2015-03-21 09:46:15 +0100

    alles

---
+++ desktop/src/nl/royenedwin/gamejam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.royenedwin.gamejam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.royenedwin.gamejam.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
