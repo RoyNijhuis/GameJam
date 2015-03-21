8d215e5adfeb1460e4fa6af980d0b9890baa06ea
EdwinFl <e.t.flapper@student.utwente.nl>
2015-03-21 14:37:26 +0100

    asdfasd

--- core/src/nl/royenedwin/gamejam/Mario.java
+++ core/src/nl/royenedwin/gamejam/Mario.java
@@ -104,7 +104,8 @@ public class Mario implements MoveableObject{
 			
 		}
 		if(falling){
-			if(!field.fieldIsFull((int)(location.x/32), (int)(location.y/32) -1)) {
+			if(!field.fieldIsFull((int)(location.x/32), (int)(location.y/32) -1)&&
+					!field.fieldIsFull((int)((location.x+31)/32), (int)(location.y/32) + sizeY+1)) {
 				location.y-=speedVer;
 				if(speedVer<=7){
 					speedVer+=0.2;
